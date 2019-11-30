f55c5c5b4ec3e0aa29ce4179c77bb1d55739b435
Dewald Abrie <dewald@rentalcover.com>
2019-11-30 13:24:43 +1100

    Separate encoding from image formatting

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='face_recognition',
-     version='0.1',
-     scripts=['face_recognition'],
+     name='face_recog_light',
+     version='0.2',
+     scripts=['face_recog_light/face_recognition.py'],
      author="Dewald Abrie",
      author_email="dewaldabrie@gmail.com",
      description="light-weight face recognition",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/dewaldabrie/face_recognition",
-     packages=setuptools.find_packages(),
-     package_data={'': ['models/facenet.tflite']},
+     packages=['face_recog_light'],
+     package_dir={'face_recog_light': 'face_recog_light'},
+     package_data={'face_recog_light': ['models/facenet.tflite']},
      include_package_data=True,
      classifiers=[
          "Programming Language :: Python :: 3",
